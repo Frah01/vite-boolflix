b2b5b02b6315ffdd5adfb990afd6e6ea252db126
Frah01 <vinario.francesco@gmail.com>
2023-01-17 17:46:42 +0100

    creato app content

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,24 @@
 <script>
+import { store } from './store'
+import AppContent from './components/AppContent.vue';
 export default {
-  
+  components: {
+    AppContent,
+  },
+  data() {
+    return {
+      store,
+    }
+  }
 }
 </script>
+
 <template lang="">
-  <div>
-    
-  </div>
+<div>
+  <AppContent />
+</div>
 </template>
-<style lang="">
-  
+
+<style lang="scss">
+@use './styles/general.scss'
 </style>
--- src/components/AppContent.vue
+++ src/components/AppContent.vue
@@ -1,13 +1,37 @@
 <script>
+import { store } from '../store';
+import AppCard from './AppCard.vue'
+import axios from 'axios';
 export default {
-    
+    components: {
+        AppCard,
+    },
+    name: 'AppMain',
+    data() {
+        return {
+            store,
+            inputText: '',
+        }
+    },
+   
 }
 </script>
 <template lang="">
-    <div>
-        
+    <div class="container my-4">
+        <div class="row">
+            <div class="input-group mb-3">
+                <input type="text" class="form-control" placeholder="Ricerca il film" aria-label="Ricerca il film" v-model="inputText" @keyup.enter="search(inputText)">
+                <button class="btn btn-outline-secondary" type="button" @click="search(inputText)">Cerca</button>
+            </div>
+        </div>
+        <div class="mb-5">
+             {{ store.movieArr.length }} film trovati
+        </div>
+        <div class="row row-cols-5 gap-5 justify-content-between">
+            <AppSingleCard v-for="(item, index) in store.movieArr" :key="index" :movie="item" />
+        </div>
     </div>
 </template>
-<style lang="">
-    
+<style lang="scss" scoped>
+
 </style>
