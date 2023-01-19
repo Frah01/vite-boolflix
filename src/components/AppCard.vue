f93fdb3b8e7a8217fb91974fb31f247d4272fcdc
Frah01 <vinario.francesco@gmail.com>
2023-01-19 23:29:31 +0100

    Aggiunti effetti hover alle cards

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -72,23 +72,28 @@ export default {
 </script>
 
 <template lang="">
-<div class="card-container" @mouseenter="hover = true" @mouseleave="hover = false" >
-  <img class="card-img" :src="Image(info)" :alt="title(info)">
-  <div class="card-body" v-if="hover">
-    <h5 class="card-title">{{ title(info) }}</h5>
-    <p class="card-text">Titolo Originale: {{  originalTitle(info) }}</p>
+   <div class="cards" @mouseenter="hover = true" @mouseleave="hover = false"><img class="copertina"  :src="Image(info)" :alt="title(info)"/>
+    <div class="info" v-if="hover">
+      <h5 class="card-title">{{ title(info) }}</h5>
+      <p class="card-text">Titolo Originale: {{  originalTitle(info) }}</p>
     <p class="card-text">Voto:  
       <i v-for="star in stelle" class="fa-solid fa-star stars-color"></i>
       <i v-for="star in stelleVuote" class="fa-regular fa-star stars-empty-color"></i>
     </p>
-    <p 
-    class="card-text d-flex">Lingua: {{ info.original_language.toUpperCase() }}
-    <img class="language"  :alt="`${info.original_language}`" :src="`https://unpkg.com/language-icons/icons/${info.original_language}.svg`">
+    <p class="card-text d-flex">Lingua: {{ info.original_language.toUpperCase() }}
+    
     </p>
+    <!-- <img class="language"  :alt="`${info.original_language}`" :src="`https://unpkg.com/language-icons/icons/${info.original_language}.svg`"> -->
     <p class="card-text">Overview: {{ overview() }}</p>
+    </div>
+  </div>
+<!-- <div class="card-container"  >
+  <div class="card-body">
+   
+    
     
   </div>
-</div>
+</div> -->
 </template>
 
 <style lang="scss" scoped>
@@ -113,8 +118,7 @@ export default {
     
     .language{
       margin-left: 1em;
-      display: block;
-      width: 30px;
+      width: 20px;
     }
     
   }
@@ -126,7 +130,67 @@ export default {
     color: rgb(172, 172, 172);
   }
 
-  .card-img {
-    min-width: 100%;
+  .copertina  {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    -o-object-fit: cover;
+     object-fit: cover;
+      
+
   }
+
+  .cards {
+  width: 305px;
+  height: 420px;
+  padding: 1.5rem;
+  background: white;
+  position: relative;
+  display: flex;
+  align-items: flex-end;
+  transition: 0.4s ease-out;
+  box-shadow: 0px 7px 10px rgba(0, 0, 0, 0.5);
+}
+.cards:hover {
+  transform: translateY(-20px);
+}
+.cards:hover:before {
+  opacity: 1;
+}
+.cards:hover .info {
+  opacity: 1;
+  transform: translateY(0px);
+}
+.cards:before {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: block;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.6);
+  z-index: 2;
+  transition: 0.5s;
+  opacity: 0;
+}
+.cards .info {
+  position: relative;
+  z-index: 3;
+  color: white;
+  opacity: 0;
+  transform: translateY(30px);
+  transition: 0.5s;
+}
+.cards .info h1 {
+  margin: 0px;
+}
+.cards .info p {
+  letter-spacing: 1px;
+  font-size: 15px;
+  margin-top: 8px;
+}
+
 </style>
