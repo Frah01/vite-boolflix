458425f087552480ebd5fadfeaf4f9621f6d0de6
Frah01 <vinario.francesco@gmail.com>
2023-01-19 23:41:06 +0100

    Pulito codice

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -103,19 +103,6 @@ export default {
   min-width: 300px;
   padding: 1px;
   position: relative;
-  .card-body{
-      padding: 20px;
-      height: 100%;
-      position: absolute;
-      bottom: 0;
-      left: 0;
-      display: flex;
-      flex-direction: column;
-      justify-content: flex-end;
-      color: white;
-      background: linear-gradient(0deg, rgba(0,0,0,1) 0%, rgba(255,255,255,0) 100%);
-    }
-    
     .language{
       margin-left: 1em;
       width: 20px;
